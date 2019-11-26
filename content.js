bb9b50d97c60438de126bb4cbfa2f64e70c6fe21
Vinesh Persaud <vineshpersaud@gmail.com>
2019-11-25 23:35:31 -0500

    beta version working

---
+++ content.js
@@ -0,0 +1,28 @@
+
+let btn = document.createElement("BUTTON");
+btn.innerHTML = "Hide SideBar";
+btn.setAttribute("id", "hide_button");
+btn.onclick = function functionName() {
+  hideSidebar()
+}
+
+
+const header = document.getElementsByClassName("flex-grid flex-grid--center-y")[0]
+
+header.appendChild(btn);
+
+
+function hideSidebar() {
+  let button = document.getElementById("hide_button")
+  if (button.innerHTML === "Hide SideBar" ){
+  document.getElementsByClassName("site-sidebar")[0].style.width = "0px";
+  document.getElementsByClassName("site-main")[0].style.width = "100%";
+
+  button.innerHTML = "Show SideBar"
+  }
+  else {
+    button.innerHTML = "Hide SideBar"
+    document.getElementsByClassName("site-sidebar")[0].style.width = "263px";
+    document.getElementsByClassName("site-main")[0].style.width = "93%";
+  }
+}
